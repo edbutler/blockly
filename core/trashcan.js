da5abd3177a6e26f0d7c8a542ad0a0f713d875af
Catalina <alvarezruizc@carleton.edu>
2020-12-20 13:36:32 -0500

    reverting last

--- core/trashcan.js
+++ core/trashcan.js
@@ -67,7 +67,7 @@ Blockly.Trashcan.prototype.LID_HEIGHT_ = 12;
  * @type {number}
  * @private
  */
-Blockly.Trashcan.prototype.MARGIN_BOTTOM_ = 100;
+Blockly.Trashcan.prototype.MARGIN_BOTTOM_ = 200;
 
 /**
  * Distance between trashcan and right edge of workspace.
