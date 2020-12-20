aa7f47fedb1e9c1d15b44de3da24e12026c3302b
Catalina <alvarezruizc@carleton.edu>
2020-12-20 14:05:53 -0500

    reduced bottom margin trashcan

--- core/trashcan.js
+++ core/trashcan.js
@@ -67,7 +67,7 @@ Blockly.Trashcan.prototype.LID_HEIGHT_ = 16;
  * @type {number}
  * @private
  */
-Blockly.Trashcan.prototype.MARGIN_BOTTOM_ = 50;
+Blockly.Trashcan.prototype.MARGIN_BOTTOM_ = 10;
 
 /**
  * Distance between trashcan and right edge of workspace.
