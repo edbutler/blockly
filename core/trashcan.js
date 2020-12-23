076d4dafc6ca6daca8e1552229f7e517009f69f2
Catalina <alvarezruizc@carleton.edu>
2020-12-23 18:22:44 -0500

    changed trashcan location

--- core/trashcan.js
+++ core/trashcan.js
@@ -67,7 +67,7 @@ Blockly.Trashcan.prototype.LID_HEIGHT_ = 16;
  * @type {number}
  * @private
  */
-Blockly.Trashcan.prototype.MARGIN_BOTTOM_ = 5;
+Blockly.Trashcan.prototype.MARGIN_BOTTOM_ = 0;
 
 /**
  * Distance between trashcan and right edge of workspace.
